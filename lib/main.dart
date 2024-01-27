303f647cdabd0ab44b12aa298058d84a44e34399
kumarrahul771996 <kumarrahul771996@gmail.com>
2024-01-27 18:19:54 +0000

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Xylophone',
+      home: HelloWorld(),
+    );
+  }
+}
+
+class HelloWorld extends StatelessWidget {
+  const HelloWorld({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text("Xylophone"),
+          centerTitle: true,
+        ),
+        body: const Center(child: Text("Hello World")),
+      ),
+    );
+  }
+}
