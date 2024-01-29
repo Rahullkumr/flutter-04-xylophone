346cebecbf8f6343406eb3ae713b2de2d74e22b7
kumarrahul771996 <kumarrahul771996@gmail.com>
2024-01-29 01:52:04 +0000

    fix: audio play in web version

--- lib/main.dart
+++ lib/main.dart
@@ -2,48 +2,35 @@ import 'package:flutter/material.dart';
 import 'package:audioplayers/audioplayers.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
+
+  final player = AudioPlayer();
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Xylophone',
-      home: Xylophone(),
-    );
-  }
-}
-
-class Xylophone extends StatefulWidget {
-  const Xylophone({super.key});
-
-  @override
-  State<Xylophone> createState() => _XylophoneState();
-}
-
-class _XylophoneState extends State<Xylophone> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.black,
-      appBar: AppBar(
-        title: const Text("Xylophone"),
-        centerTitle: true,
-      ),
-      body: SafeArea(
-        child: Center(
-          child: TextButton(
-            onPressed: () async {
-              final player = AudioPlayer();
-              await player.play(
-                AssetSource('audio/note1.wav'),
-              );
-            },
-            child: const Text("Click me"),
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          title: const Text("Xylophone"),
+          centerTitle: true,
+        ),
+        body: SafeArea(
+          child: Center(
+            child: TextButton(
+              onPressed: ()  {
+                 player.play(
+                  AssetSource('note1.wav'),
+                );
+              },
+              child: const Text("Click me"),
+            ),
           ),
         ),
       ),
